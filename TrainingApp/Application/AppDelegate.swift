bc2f8e433c840d97e9ccf9856bab9cc752d7c712
Moiseev Andrei <andreimoiseew2009@yandex.ru>
2021-12-16 16:06:00 +0300

    Добавлена CoreData

--- TrainingApp/Application/AppDelegate.swift
+++ TrainingApp/Application/AppDelegate.swift
@@ -10,6 +10,8 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var coreDataStack = CoreDataStack()
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
---
+++ TrainingApp/Core/Common/Models/CoreDataModels/CDCategories+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  CDCategories+CoreDataClass.swift
+//  
+//
+//  Created by Андрей Моисеев on 13.11.2021.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(CDCategories)
+public class CDCategories: NSManagedObject {
+
+}
